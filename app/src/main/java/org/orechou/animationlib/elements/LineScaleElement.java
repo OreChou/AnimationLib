07b1fa596f2dbb3ff505ed94371fc1c9b7614791
unknown <11104387@vivo.xyz>
2019-08-22 18:00:05 +0800

    测试 git

---
+++ app/src/main/java/org/orechou/animationlib/elements/LineScaleElement.java
@@ -0,0 +1,18 @@
+package org.orechou.animationlib.elements;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+import org.orechou.animationlib.Element;
+
+public class LineScaleElement extends Element {
+    @Override
+    protected void draw(Canvas canvas, Paint paint) {
+
+    }
+
+    @Override
+    protected void onCreateAnimators() {
+
+    }
+}
